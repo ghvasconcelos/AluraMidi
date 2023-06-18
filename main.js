e8ff448ecc930435b7a8b554db5c0313f4fb293f
ghvasconcelos <ghvasconcelos@hotmail.com>
2023-06-17 22:02:06 -0300

    Alura Midi final

--- main.js
+++ main.js
@@ -1,21 +1,39 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play()
-};
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        //alert('Elemento não encontrado');
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
+
+}
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
 
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
 
-while (contador < listaDeTeclas.length) {
+    tecla.onkeydown = function (evento) {
 
-    listaDeTeclas[contador].onclick = () => {
-        tocaSom('')
-    };
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
 
-    contador = contador + 1;
+    }
 
-    console.log(contador)
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 
 }
